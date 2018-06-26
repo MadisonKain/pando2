5bb17a04bc0ab1fcd0c806837b9bf41bb16e8721
Madison Kain <madisonlkain@gmail.com>
2018-06-25 20:11:15 -0600

    Started Nav styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,6 +3,8 @@ import "./Nav.css";
 // ========== Redux Imports ========== //
 import { connect } from "react-redux";
 import { getUserInfoFromServer } from "../../ducks/reducer";
+// ========== Other Imports ========= //
+import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
     
@@ -11,21 +13,30 @@ class Nav extends React.Component {
     }
 
     render(){
+
         const navBarConditionalJSX = this.props.user.userName ? 
         (
-            <nav>
-                <h1>LoggedIn</h1>
-            </nav>
+            <div className="flex-column">
+                <Link to="/"><p>Home</p></Link>
+                <Link to="/shop"><p>Shop</p></Link>
+            </div>
         )
         :
         (
-            <nav>
-                <h1>NOTLoggedIn</h1>
-            </nav>
+            <div className="flex-row-between">
+                <Link to="/"><p>Home</p></Link>
+                <Link to="/shop"><p>Shop</p></Link>
+                <Link to={`/profile/${this.props.user.id}`}><p>Profile</p></Link>
+                <Link to="/cart"><p>Cart</p></Link>
+            </div>
         )
+
         return(
             <div>
-                { navBarConditionalJSX }
+                <nav className="flex-row-between">
+                    <h2 style={{"padding": "0 0 0 30px"}}>Pando</h2>
+                    { navBarConditionalJSX }
+                </nav>
             </div>
         )
     }
