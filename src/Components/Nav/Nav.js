82b38b509de2d9e332d13b035f9b15bec7bd408f
Madison Kain <madisonlkain@gmail.com>
2018-06-25 21:11:12 -0600

    Working out the color scheme? Blue and Orange atm

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -14,16 +14,16 @@ class Nav extends React.Component {
 
     render(){
 
-        const navBarConditionalJSX = this.props.user.userName ? 
+        const navBarConditionalJSX = !this.props.user.userName ? 
         (
-            <div className="flex-column">
+            <div className="conditionalNavBar" style={{"padding": "0 40px 0 0"}}>
                 <Link to="/"><p>Home</p></Link>
                 <Link to="/shop"><p>Shop</p></Link>
             </div>
         )
         :
         (
-            <div className="flex-row-between">
+            <div className="conditionalNavBar" style={{"padding": "0 40px 0 0"}}>
                 <Link to="/"><p>Home</p></Link>
                 <Link to="/shop"><p>Shop</p></Link>
                 <Link to={`/profile/${this.props.user.id}`}><p>Profile</p></Link>
@@ -33,8 +33,8 @@ class Nav extends React.Component {
 
         return(
             <div>
-                <nav className="flex-row-between">
-                    <h2 style={{"padding": "0 0 0 30px"}}>Pando</h2>
+                <nav className="navBarContainer">
+                    <h2 className="navTitle">Pando</h2>
                     { navBarConditionalJSX }
                 </nav>
             </div>
